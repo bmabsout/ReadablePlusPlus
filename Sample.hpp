21db1daf2b84ad84bdb900b5c7d45b615f67bd12
Bassel Mabsout <bme11@mail.aub.edu>
2014-06-30 00:11:38 +0300

    added samples of changes that will happen with readablePlusPlus

---
+++ Sample.hpp
@@ -0,0 +1,11 @@
+class Sample
+{
+public:
+	int* a,*b,*c,*d;
+	void constructorOverloading(int first, int second, float third);
+	Sample();
+	Sample(int first, int second, float third);
+
+void doStuff(int yes);
+
+};
